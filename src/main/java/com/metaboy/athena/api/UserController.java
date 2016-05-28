de782b01ca41d17067cf30103188e1bb62370b56
yuxiong.wangyx <yxiong.wang@gmail.com>
2016-05-28 22:51:45 +0800

    restapi service,为athena-web等客户端提供api.

--- src/main/java/com/metaboy/athena/api/UserController.java
+++ src/main/java/com/metaboy/athena/api/UserController.java
@@ -4,11 +4,14 @@ import com.google.common.base.CharMatcher;
 import com.metaboy.athena.model.RestResult;
 import com.metaboy.athena.model.User;
 import com.metaboy.athena.service.UserService;
+import com.metaboy.athena.utils.DecryptionUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -18,12 +21,15 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 
 @Controller
+@RequestMapping(value = "/api")
 public class UserController {
+
+    private static final Log LOGGER = LogFactory.getLog(UserController.class);
 
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
     public RestResult userAdd(@RequestBody User user) {
         checkNotNull(user.getUserName(), "User name is required.");
         checkNotNull(user.getPasswd(), "Password is required.");
@@ -35,6 +41,54 @@ public class UserController {
 
 
         Long id = userService.addUser(user);
-        return RestResult.ok("add user success", id);
+        if (id > 0) {
+            return RestResult.ok("add user success", id);
+        } else {
+            return RestResult.fail("add user failed", null);
+        }
+    }
+
+
+    @RequestMapping(value = "/user/{name:.*}", method = RequestMethod.PUT)
+    public RestResult userChecked(@PathVariable(value = "name") String name,
+                                  @RequestBody User user) {
+
+        user.setUserName(name);
+        user.setGmtModified(new Date());
+
+        int n = userService.modifyUserInfo(user);
+
+        if (n == 0) {
+            return RestResult.fail("User not found.", null);
+        } else if (n == 1) {
+            return RestResult.ok("Update user success.", null);
+        } else {
+            return RestResult.fail("Update user failed.", null);
+        }
+    }
+
+    @RequestMapping(value = "/user/{name:.*}", method = RequestMethod.GET)
+    public RestResult userDetails(
+            @PathVariable(value = "name") String name) {
+
+        User user = userService.getUserByUser(name);
+        if (user == null) {
+            return RestResult.fail(String.format("User[%s] Is Not Exist.", name), null);
+        } else {
+            return RestResult.ok("", user);
+        }
+    }
+
+    @RequestMapping(value = "/user/{name:.*}", method = RequestMethod.DELETE)
+    public RestResult userDelete(
+            @PathVariable(value = "name") String name) {
+
+        User user = userService.getUserByUser(name);
+        int deleteRows = userService.deleteUser(user.getId());
+        if (deleteRows == 0) {
+            return RestResult.fail(String.format("User[%s] Is Not Exist.", name), null);
+        } else {
+            return RestResult.ok("Delete user success.", null);
+        }
     }
 }
